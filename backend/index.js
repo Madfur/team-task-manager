5e9877635bfe57a824c35776eb1def7f136be9dd
Rachit2726 <kashyaprachit027@gmail.com>
2026-05-21 01:24:00 +0530

    Fixed deployment issues

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
 // Health check
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
